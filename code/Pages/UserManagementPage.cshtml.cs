e41df5df4b7ce72f482e44658f08524d7febfd51
jakino22 <146848411+jakino22@users.noreply.github.com>
2023-12-22 15:02:55 +0100

    Add files via upload

--- code/Pages/UserManagementPage.cshtml.cs
+++ code/Pages/UserManagementPage.cshtml.cs
@@ -3,30 +3,40 @@ using code.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Authentication.Cookies;
-
+using Microsoft.AspNetCore.Authentication;
+using code.Auth;
+using System.Data;
 
 namespace code.Pages;
 
 public class ManageModel : PageModel
 {
     private readonly ILogger<ManageModel> _logger;
-    public Account Curr{get;set;}
+    public bool allow;
 
-    public ManageModel(ILogger<ManageModel> logger, Account C)
+    public ManageModel(ILogger<ManageModel> logger)
     {
         _logger = logger;
-        Curr = C;
+        allow = false;
     }
     
 
     public Account? Acc{get;private set;}
-    public void OnGet()
+    public async Task OnGet()
     {
 
-        Curr.Id = 1;
-        Curr.Privileges = 1;
-        //Response.Cookies.Append("priv", "1");
+        if (!HttpContext.User.Identity.IsAuthenticated)
+        {
+            await HttpContext.ChallengeAsync();
+        }
+        var a = HttpContext.User.Claims.First(x => x.Type=="Privileges");
+        var b = AuthRequirementHandler.isBitSet(a.Value,1);//index od nuly
+        if(b){
+            allow = true;
+        }
+
     }
+
 
 }
 
